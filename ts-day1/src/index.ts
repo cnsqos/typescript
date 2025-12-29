d855da4736b2abe9f96277f276be70e9a36ccad3
cnsqo <cnsqo0815@gmail.com>
2025-12-29 20:15:23 +0900

    add
    add

---
+++ ts-day1/src/index.ts
@@ -0,0 +1,20 @@
+//1. Boolean
+//논리적인 참/ 거짓을 나타낸다.
+
+let isStudent : boolean = true;
+let hasLicense : boolean = false;
+
+//isStudent = "yes"; 문자열을  boolean타입 변수에 할당할 수 없다.
+
+console.log(`학생여부:  ${isStudent}`);
+
+
+//2. Number(숫자)
+//정수 : 소수점이 없는 숫자
+//실수 : 소수점이 있는 숫자
+//Infinity : 무한대
+//NaN(Not a Number)
+
+let age : number = 28;
+let salary : number = 55000.50;
+
